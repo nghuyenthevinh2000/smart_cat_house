e97da4494c5867b95765a64f4925a5333a7bfe2b
LongPhanPip <phanlonghd5200@gmail.com>
2021-05-26 12:06:09 +0700

    Extends class component

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, Component from 'react';
 import {withFirebase} from '../Firebase';
 import {NavLink} from 'react-bootstrap';
 
-const SignOutLink = ({firebase}) =>
-  <NavLink onClick={firebase.doSignOut}>
-    Sign Out
-  </NavLink>
+class SignOutLink extends Component {
+  render() {
+    return (
+      <NavLink onClick={this.props.firebase.doSignOut}>
+        Sign Out
+      </NavLink>
+    );
+  }
+}
 
 export default withFirebase(SignOutLink);
