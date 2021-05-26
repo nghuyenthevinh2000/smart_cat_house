2f5676a8d29dd2c6f6b78801acf366c324085524
LongPhanPip <phanlonghd5200@gmail.com>
2021-05-26 11:09:59 +0700

    Change footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 
 import {Container} from 'react-bootstrap';
 
+
 class Footer extends Component {
   render() {
     return (
       <footer className="text-center text-muted fixed-bottom p-4">
         <Container>
-          © 2021 Copyright: Group 10 IoT
+          © 2021 Copyright: Group 10 IoT Smart cat home
         </Container>
       </footer>
     );
